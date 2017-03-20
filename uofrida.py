74e15fecc01e72546e5673a7334eacfbc09c2929
uosa <uosa@none.net>
2017-03-20 04:53:30 +0100

    Added config.py

--- uofrida.py
+++ uofrida.py
@@ -2,12 +2,8 @@ import frida;
 import sys;
 import os;
 
-#
-# Change those two directories to your UO install and where
-# you put those uo-frida files.
-#
-uo_dir = "C:\\Program Files (x86)\\Electronic Arts\\Ultima Online Enhanced"
-uo_frida_dir = uo_dir + "\\uofrida"
+from config.config import uo_dir;
+from config.config import uo_frida_dir;
 
 script_content = ''
 
